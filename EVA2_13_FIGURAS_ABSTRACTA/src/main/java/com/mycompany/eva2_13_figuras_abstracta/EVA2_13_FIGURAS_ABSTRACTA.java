ed4f057db7f6b9a5b2bb214d72410cf5e23c65ee
DEVM0TECNM <124701514+DEVM0TECNM@users.noreply.github.com>
2023-03-17 09:38:32 -0600

    Add files via upload

---
+++ EVA2_13_FIGURAS_ABSTRACTA/src/main/java/com/mycompany/eva2_13_figuras_abstracta/EVA2_13_FIGURAS_ABSTRACTA.java
@@ -0,0 +1,9 @@
+package com.mycompany.eva2_13_figuras_abstracta;
+
+public class EVA2_13_FIGURAS_ABSTRACTA {
+
+   public static void main(String[] args) {
+        //Figuras figura = new Figuras(); NO SE PUEDEN CREAR OBJETOS
+        Circulo circulo = new Circulo();
+ }
+}
---
+++ EVA2_13_FIGURAS_ABSTRACTA/src/main/java/com/mycompany/eva2_13_figuras_abstracta/Figuras.java
@@ -0,0 +1,7 @@
+package com.mycompany.eva2_13_figuras_abstracta;
+
+abstract public class Figuras {
+    public abstract double calcularArea();
+
+    public abstract double calcularPeri();
+}
