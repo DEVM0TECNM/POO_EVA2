8ea9620feb880c99c14f1a59f96bb08fb3cd2473
DEVM0TECNM <124701514+DEVM0TECNM@users.noreply.github.com>
2023-03-27 09:44:18 -0600

    Add files via upload

---
+++ EVA2_17_INTERFACES/src/main/java/com/mycompany/eva2_17_interfaces/MostrarDatos.java
@@ -0,0 +1,7 @@
+package com.mycompany.eva2_17_interfaces;
+
+public interface MostrarDatos {
+    
+    public abstract void imprimirDatos();
+    
+}
---
+++ EVA2_17_INTERFACES/src/main/java/com/mycompany/eva2_17_interfaces/Producto.java
@@ -0,0 +1,31 @@
+package com.mycompany.eva2_17_interfaces;
+
+public abstract class Producto implements MostrarDatos {
+    
+    private double Precio;
+    private String Nombre;
+
+    public Producto() {
+    }
+
+    public Producto(double Precio, String Nombre) {
+        this.Precio = Precio;
+        this.Nombre = Nombre;
+    }
+
+    public double getPrecio() {
+        return Precio;
+    }
+
+    public void setPrecio(double Precio) {
+        this.Precio = Precio;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+}
