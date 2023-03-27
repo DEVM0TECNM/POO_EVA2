546621cc27746ff4a7922bbba3f9df6b1008417d
DEVM0TECNM <124701514+DEVM0TECNM@users.noreply.github.com>
2023-03-27 09:44:46 -0600

    Add files via upload

---
+++ EVA2_18_INTERFACES_HERENCIA/src/main/java/com/mycompany/eva2_18_interfaces_herencia/EVA2_18_INTERFACES_HERENCIA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_18_interfaces_herencia;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA2_18_INTERFACES_HERENCIA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
