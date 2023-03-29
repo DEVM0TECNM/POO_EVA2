b9bcc80ff2c56d30978191d57bb06cfeb94e80ae
Arturo Gerardo Banderas Sandoval <124701514+DEVM0TECNM@users.noreply.github.com>
2023-03-29 09:29:25 -0600

    Add files via upload

---
+++ EVA2_19_INTERFACES_FIGURAS/src/eva2_19_interfaces_figuras/Figuras.java
@@ -0,0 +1,11 @@
+package eva2_19_interfaces_figuras;
+
+/**
+ *
+ * @author Usuario
+ */
+public interface Figuras {
+    public abstract double calcularArea();
+    double calcularPeri();
+}
+
---
+++ EVA2_19_INTERFACES_FIGURAS/src/eva2_19_interfaces_figuras/MostrarDatos.java
@@ -0,0 +1,9 @@
+package eva2_19_interfaces_figuras;
+
+/**
+ *
+ * @author Usuario
+ */
+public interface MostrarDatos {
+    void imprimirDatos();
+}
